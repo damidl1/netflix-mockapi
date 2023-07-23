6477ca2a8249407e30b24a3b52dfb12165fef087
Damiano <d.dilionardo@gmail.com>
2023-07-23 16:29:53 +0200

    continuazione task

--- controllers/app-controller.js
+++ controllers/app-controller.js
@@ -4,8 +4,7 @@ class AppController{
 
         this.shows = [];
         this.isVoting = false;
-        this.floatingInput = document.getElementById('floating-input');
-        this.newShowInput = document.getElementById('newShowInput');
+        this.orderMethod = 'upvote';
         // this.submitButton = document.getElementById('submitButton');
         // this.submitButton.addEventListener('click', () => {
         //   const title = this.newShowInput.value.trim();
@@ -31,7 +30,7 @@ class AppController{
 
        appContainer.innerHTML = `
        <header>
-       <h1>Netflix Score</h1>
+      
        <a href= "./index.html">lista</a>
        <a href= "./new-show.html">nuovo</a>
      </header>
@@ -42,7 +41,7 @@ class AppController{
      </main>
      
      <footer>
-       <p>I diritti sono tutti miei!!!</p>
+       <p>Sito creato da damidl ©, 2023</p>
      </footer>
      `
     
@@ -50,7 +49,21 @@ class AppController{
     }
 
 
+    selectOrder(orderMethod){
+      this.orderMethod = orderMethod;
+      this.renderShows();
+    }
 
+    updateOrderShows() {
+      if (this.orderMethod === 'upvote') {
+          return this.shows.sort((s1, s2) => s2.upVotes - s1.upVotes);
+      } else if (this.orderMethod === 'downVote') {
+          return this.shows.sort((s1, s2) => s2.downVotes - s1.downVotes);
+      }
+      return this.shows;
+  }
+
+  
 
 
     renderShows(){
@@ -68,11 +81,13 @@ class AppController{
       sortUpButton.appendChild(document.createTextNode('Ordina per upvotes'));
       sortUpButton.addEventListener('click', () => this.sortByUpvotes());
       btnContainer.appendChild(sortUpButton);
+      sortUpButton.classList.add('sortUpBtn');
 
       const sortDownButton = document.createElement('button');
       sortDownButton.appendChild(document.createTextNode('Ordina per downvotes'));
       sortDownButton.addEventListener('click', () => this.sortByDownvotes());
       btnContainer.appendChild(sortDownButton);
+      sortDownButton.classList.add('sortDownBtn');
 
       const showsContainer = document.getElementById('shows-container');
       showsContainer.innerHTML = '';
@@ -100,6 +115,7 @@ class AppController{
 
         const downVotesSpan = document.createElement('span');
         downVotesSpan.appendChild(document.createTextNode(show.downVotes));
+        downVotesSpan.classList.add('downVotesSpan');
         listElement.appendChild(downVotesSpan);
         
 
@@ -176,16 +192,16 @@ class AppController{
       this.renderShows();
     }
 
-    showFloatingInput(){
+    // showFloatingInput(){
 
-      this.floatingInput.style.display = 'block';
-    }
+    //   this.floatingInput.style.display = 'block';
+    // }
 
-    hideFloatingInput(){
+    // hideFloatingInput(){
 
-      this.floatingInput.style.display = 'none';
-      this.newShowInput.value = '';
-    }
+    //   this.floatingInput.style.display = 'none';
+    //   this.newShowInput.value = '';
+    // }
 
  
 }
